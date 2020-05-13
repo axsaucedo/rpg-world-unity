7720fed4419901b5b5919e86453f81ffc005da80
Alejandro Saucedo <axsauze@gmail.com>
2020-05-13 20:15:59 +0100

    Added animations and movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -9,6 +9,29 @@ public class Mover : MonoBehaviour
 
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;        
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+        UpdateAnimator();
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hit.point;        
+        }
+    }
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
