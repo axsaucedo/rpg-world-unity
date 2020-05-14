e1ced32f949c59e1b5e0b02027e22719a111e049
Alejandro Saucedo <axsauze@gmail.com>
2020-05-14 07:53:38 +0100

    Added move to attack with range

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,12 +1,33 @@
+using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Combat
-{
+{        
     public class Fighter : MonoBehaviour 
     {
-        public void Attack(CombatTarget target)
+        [SerializeField] float weaponRange = 2f;
+
+        Transform target;
+
+        private void Update() {
+            if (target != null)
+            {
+                bool isInRange = Vector3.Distance(transform.position, target.position) < weaponRange;
+                Mover mover = GetComponent<Mover>();
+                if (!isInRange)
+                {
+                    mover.MoveTo(target.position);
+                }
+                else
+                {
+                    mover.Stop();
+                }
+            }
+        }
+
+        public void Attack(CombatTarget combatTarget)
         {
-            print("Hello");
+            target = combatTarget.transform;
         }
     }
 }
