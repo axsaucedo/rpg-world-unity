5e3a62f72b903c44b709e10c664545f177183fb0
Alejandro Saucedo <axsauze@gmail.com>
2020-05-14 18:41:02 +0100

    Added time between attacks

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,15 +1,22 @@
+using RPG.Core;
 using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Combat
-{        
-    public class Fighter : MonoBehaviour 
+{
+    public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
 
         Transform target;
 
-        private void Update() {
+        float timeSinceLastAttack = 0;
+
+        private void Update()
+        {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target != null)
             {
                 bool isInRange = Vector3.Distance(transform.position, target.position) < weaponRange;
@@ -20,14 +27,35 @@ namespace RPG.Combat
                 }
                 else
                 {
-                    mover.Stop();
+                    mover.Cancel();
+                    AttackBehaviour();
                 }
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
             }
         }
 
         public void Attack(CombatTarget combatTarget)
         {
+            GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.transform;
+        }
+
+        public void Cancel()
+        {
+            target = null;
+        }
+
+        void Hit()
+        {
+
         }
     }
 }
