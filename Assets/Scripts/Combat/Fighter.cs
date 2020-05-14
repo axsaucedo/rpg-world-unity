4cf0bdc4e26bbdb010b14677342a2c9b37ffc09b
Alejandro Saucedo <axsauze@gmail.com>
2020-05-14 18:48:05 +0100

    Added health

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -8,6 +8,7 @@ namespace RPG.Combat
     {
         [SerializeField] float weaponRange = 2f;
         [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
 
         Transform target;
 
@@ -37,9 +38,17 @@ namespace RPG.Combat
         {
             if (timeSinceLastAttack > timeBetweenAttacks)
             {
+                // This will trigger the Hit() event
                 GetComponent<Animator>().SetTrigger("attack");
                 timeSinceLastAttack = 0;
             }
+        }
+        
+        // Animation event
+        void Hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
 
         public void Attack(CombatTarget combatTarget)
@@ -51,11 +60,6 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
-        }
-
-        void Hit()
-        {
-
         }
     }
 }
