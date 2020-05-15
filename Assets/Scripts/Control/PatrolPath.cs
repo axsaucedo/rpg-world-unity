95d6835aa688d45d8cfcb0c46741ba0a6cdf923a
Alejandro Saucedo <axsauze@gmail.com>
2020-05-15 08:30:02 +0100

    Added waypoints

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PatrolPath : MonoBehaviour
+namespace RPG.Control
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PatrolPath : MonoBehaviour
     {
-        
-    }
+        private void OnDrawGizmos()
+        {
+            const float waypointGizmoRaidius = 0.3f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRaidius);
+            }
+        }
     }
 }
