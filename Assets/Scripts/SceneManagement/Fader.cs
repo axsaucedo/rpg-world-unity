7cfc80331c602a2b24ac87c66b228372133b0b5b
Alejandro Saucedo <axsauze@gmail.com>
2020-05-17 11:37:52 +0100

    Created fader in and out

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        public IEnumerator FadeOut(float totalDuration)
+        {
+            while (canvasGroup.alpha < 1f)
+            {
+                canvasGroup.alpha = Mathf.Min(canvasGroup.alpha + Time.deltaTime / totalDuration, 1f);
+                yield return null;
+            }
+        }
+
+        public IEnumerator FadeIn(float totalDuration)
+        {
+            while (canvasGroup.alpha > 0f)
+            {
+                canvasGroup.alpha = Mathf.Max(canvasGroup.alpha - Time.deltaTime / totalDuration, 0f);
+                yield return null;
+            }
+        }
+    }
+}
+
