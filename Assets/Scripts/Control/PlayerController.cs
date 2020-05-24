0b23a56deda4cfef77f093742aac593a89db8123
Alejandro Saucedo <axsauze@gmail.com>
2020-05-24 08:47:37 +0100

    Added navmesh for water

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -4,19 +4,12 @@ using UnityEngine;
 using RPG.Resources;
 using System;
 using UnityEngine.EventSystems;
+using UnityEngine.AI;
 
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
-        enum CursorType
-        {
-            None,
-            Movement,
-            Combat,
-            UI
-        }
-
         [System.Serializable]
         struct CursorMapping
         {
@@ -26,6 +19,7 @@ namespace RPG.Control
         }
 
         [SerializeField] CursorMapping[] cursorMappings = null;
+        [SerializeField] float maxNavMeshProjectionDistance = 1f;
 
         Health health;
 
@@ -46,9 +40,8 @@ namespace RPG.Control
                 SetCursor(CursorType.None);
                 return;
             }
-            if (InteractWithCombat())
+            if (InteractWithComponent())
             {
-                SetCursor(CursorType.Combat);
                 return;
             }
             if (InteractWithMovement())
@@ -56,6 +49,37 @@ namespace RPG.Control
                 SetCursor(CursorType.Movement);
                 return;
             }
+            SetCursor(CursorType.None);
+        }
+
+        private bool InteractWithComponent()
+        {
+            RaycastHit[] hits = RaycastAllSorted();
+            foreach (RaycastHit hit in hits)
+            {
+                IRaycastable[] raycastables = hit.transform.GetComponents<IRaycastable>();
+                foreach (IRaycastable raycastable in raycastables)
+                {
+                    if (raycastable.HandleRaycast(this))
+                    {
+                        SetCursor(raycastable.GetCursorType());
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        RaycastHit[] RaycastAllSorted()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            float[] distances = new float[hits.Length];
+            for (int i = 0; i < hits.Length; i++)
+            {
+                distances[i] = hits[i].distance;
+            }
+            Array.Sort(distances, hits);
+            return hits;
         }
 
         private bool InteractWithUI()
@@ -63,40 +87,38 @@ namespace RPG.Control
             return EventSystem.current.IsPointerOverGameObject();
         }
 
-        private bool InteractWithCombat()
+        private bool InteractWithMovement()
         {
-            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
-            foreach (RaycastHit hit in hits)
+            Vector3 target;
+            bool hasHit = RaycastNavMesh(out target);
+
+            if (hasHit)
             {
-                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
-
-                if (target == null) continue;
-
-                if (!GetComponent<Fighter>().CanAttack(target.gameObject)) continue;
-
-                if (Input.GetMouseButtonDown(0))
+                if (Input.GetMouseButton(0))
                 {
-                    GetComponent<Fighter>().Attack(target.gameObject);
+                    GetComponent<Mover>().StartMoveAction(target, 1f);
                 }
                 return true;
             }
             return false;
         }
 
-        private bool InteractWithMovement()
+        private bool RaycastNavMesh(out Vector3 target)
         {
+            target = new Vector3();
             RaycastHit hit;
             bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
+            if (!hasHit) return false;
 
-            if (hasHit)
-            {
-                if (Input.GetMouseButton(0))
-                {
-                    GetComponent<Mover>().StartMoveAction(hit.point, 1f);
-                }
-                return true;
-            }
-            return false;
+            NavMeshHit navMeshHit; 
+            bool hasCastToNavMesh = NavMesh.SamplePosition(
+                hit.point, out navMeshHit, maxNavMeshProjectionDistance, NavMesh.AllAreas);
+            
+            if (!hasCastToNavMesh) return false;
+
+            target = navMeshHit.position;
+
+            return true;
         }
 
         private void SetCursor(CursorType type)
