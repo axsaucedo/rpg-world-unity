7eb13299a628ef4889576f6c224092d0dc42b07f
Alejandro Saucedo <axsauze@gmail.com>
2020-05-24 11:31:30 +0100

    Added functionality to spawn damage on top of head dynamically with animation using damagetextspawner

--- Assets/Scripts/Resources/Health.cs
+++ Assets/Scripts/Resources/Health.cs
@@ -4,11 +4,14 @@ using RPG.Core;
 using RPG.Saving;
 using RPG.Stats;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace RPG.Resources
 {
     public class Health : MonoBehaviour, ISaveable
     {
+        [SerializeField] UnityEvent takeDamage;
+
         LazyValue<float> healthPoints;
         bool isDead = false;
         BaseStats baseStats;
@@ -43,10 +46,15 @@ namespace RPG.Resources
         {
             print(gameObject.name + " damage: " + damage);
             healthPoints.value = Mathf.Max(healthPoints.value - damage, 0);
+
             if (healthPoints.value == 0)
             {
                 Die();
                 AwardExperience(instigator);
+            }
+            else
+            {
+                takeDamage.Invoke();
             }
         }
 
